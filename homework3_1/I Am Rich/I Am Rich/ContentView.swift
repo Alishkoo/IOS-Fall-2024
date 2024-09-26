51945356dd0ec87f6e7000a14a4a08b7f48e580f
Alishkoo <baisholanov.alibek@gmail.com>
2024-09-27 00:10:28 +0500

    ended hw3

---
+++ homework3_1/I Am Rich/I Am Rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I Am Rich
+//
+//  Created by Alibek Baisholanov on 26.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
